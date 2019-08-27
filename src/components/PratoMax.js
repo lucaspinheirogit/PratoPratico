58da563f7fa2f7492999eed777060827d6c53db2
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2019-08-27 17:59:21 -0300

    PratoMax hooks e inline styles

--- src/components/PratoMax.js
+++ src/components/PratoMax.js
@@ -1,57 +1,30 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import FastImage from 'react-native-fast-image';
-import LinearGradient from 'react-native-linear-gradient';
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import FastImage from 'react-native-fast-image'
+import LinearGradient from 'react-native-linear-gradient'
 
-import Ingrediente from './Ingrediente';
-import { PratoImg } from '../styled-components/Imagem';
-import {
-  TituloPrato, DescricaoPrato, H4, H5
-} from '../styled-components/Texto';
-import { ScrollWrapperCenter } from '../styled-components/Wrapper';
+import Ingrediente from './Ingrediente'
+import { TituloPrato, DescricaoPrato, H4, H5 } from '../styled-components/Texto'
+import { ScrollWrapperCenter } from '../styled-components/Wrapper'
 
-export default (props) => {
-  const {
-    nome, imagem, descricao, dificuldade, modo, ingredientes
-  } = props;
-  let { tempo } = props;
+export default props => {
+  const { nome, imagem, descricao, dificuldade, modo, ingredientes } = props
+  let { tempo } = props
 
-  const date = new Date(null);
-  date.setSeconds(tempo);
-  tempo = date.toISOString().substr(11, 8);
+  const date = new Date(null)
+  date.setSeconds(tempo)
+  tempo = date.toISOString().substr(11, 8)
 
   return (
     <ScrollWrapperCenter>
-      <FastImage
-        style={{
-          width: '100%', height: 280, borderColor: '#0d2b56', borderTopWidth: 2
-        }}
-        source={{ uri: imagem }}
-        resizeMode="cover"
-      >
+      <FastImage style={styles.image} source={{ uri: imagem }} resizeMode="cover">
         <LinearGradient
           start={{ x: 0, y: 0 }}
           end={{ x: 1, y: 0 }}
           colors={['rgba(13, 43, 86, 0.75)', 'rgba(13, 43, 86, 0.1)']}
-          style={{
-            width: '100%',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-          }}
+          style={styles.gradient}
         >
-          <H4
-            style={{
-              textAlign: 'left',
-              color: '#FFF',
-              textShadowColor: '#0d2b56',
-              textShadowOffset: { width: -1, height: 1 },
-              textShadowRadius: 1,
-              padding: 5,
-            }}
-          >
-            {dificuldade}
-          </H4>
+          <H4 style={styles.dificuldade}>{dificuldade}</H4>
         </LinearGradient>
       </FastImage>
       <TituloPrato>{nome}</TituloPrato>
@@ -61,24 +34,24 @@ export default (props) => {
       <DescricaoPrato>{modo}</DescricaoPrato>
 
       <H4 style={styles.label}>Tempo de Preparo</H4>
-      <DescricaoPrato style={{ textAlign: 'center' }}>{tempo}</DescricaoPrato>
+      <DescricaoPrato style={styles.alignCenter}>{tempo}</DescricaoPrato>
 
       <H4 style={styles.label}>Ingredientes</H4>
       {ingredientes.length > 0 && (
         <View style={styles.ingredientesBox}>
           <View style={styles.ingredientesHeader}>
-            <H5 style={{ color: '#fff' }}>Nome</H5>
+            <H5 style={styles.white}>Nome</H5>
           </View>
           <View style={styles.ingredientesHeader}>
-            <H5 style={{ color: '#fff' }}>Quantidade</H5>
+            <H5 style={styles.white}>Quantidade</H5>
           </View>
           <View style={styles.ingredientesHeader}>
-            <H5 style={{ color: '#fff' }}>Unidade de Medida</H5>
+            <H5 style={styles.white}>Unidade de Medida</H5>
           </View>
         </View>
       )}
       {ingredientes.length > 0 ? (
-        ingredientes.map((i) => (
+        ingredientes.map(i => (
           <Ingrediente
             key={i.Id}
             nome={i.Nome}
@@ -87,14 +60,35 @@ export default (props) => {
           />
         ))
       ) : (
-        <H5 style={{ margin: 5, color: '#777' }}>Esse prato não possui ingredientes...</H5>
+        <H5 style={styles.noMoreMessage}>Esse prato não possui ingredientes...</H5>
       )}
-      <View style={{ marginBottom: 50 }} />
+      <View style={styles.marginBottom50} />
     </ScrollWrapperCenter>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 280,
+    borderColor: '#0d2b56',
+    borderTopWidth: 2,
+  },
+  gradient: {
+    width: '100%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  dificuldade: {
+    textAlign: 'left',
+    color: '#FFF',
+    textShadowColor: '#0d2b56',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 1,
+    padding: 5,
+  },
+  alignCenter: { textAlign: 'center' },
   label: {
     width: '100%',
     textAlign: 'center',
@@ -117,4 +111,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#305c9b',
     color: '#000',
   },
-});
+  white: { color: '#fff' },
+  noMoreMessage: { margin: 5, color: '#777' },
+  marginBottom50: { marginBottom: 50 },
+})
