50c6c02ac1323864fd220fbca856087220563e0b
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2019-08-28 07:28:54 -0300

    src/index hooks

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import { InteractionManager } from 'react-native';
-import { Provider } from 'react-redux';
+import React, { useEffect } from 'react'
+import { InteractionManager } from 'react-native'
+import { Provider } from 'react-redux'
 
-import actions from './redux/actions/UsuarioActionCreator';
-import AppContainer from './routes';
-import store from './store';
-import AsyncStorage from './util/AsyncStorage';
-import NavigationService from './util/NavigationService';
+import actions from './redux/actions/UsuarioActionCreator'
+import AppContainer from './routes'
+import store from './store'
+import AsyncStorage from './util/AsyncStorage'
+import NavigationService from './util/NavigationService'
 
-export default class App extends React.Component {
-  componentDidMount() {
+export default () => {
+  useEffect(() => {
     InteractionManager.runAfterInteractions(async () => {
-      const token = await AsyncStorage.getItem('token');
-      if (token) {
-        store.dispatch(actions.renewUsuario());
-      }
-    });
+      getToken()
+    })
+  }, [])
+
+  async function getToken() {
+    const token = await AsyncStorage.getItem('token')
+    if (token) {
+      store.dispatch(actions.renewUsuario())
+    }
   }
 
-  render() {
-    return (
-      <Provider store={store}>
-        <AppContainer
-          ref={(navigatorRef) => {
-            NavigationService.setTopLevelNavigator(navigatorRef);
-          }}
-        />
-      </Provider>
-    );
-  }
+  return (
+    <Provider store={store}>
+      <AppContainer
+        ref={navigatorRef => {
+          NavigationService.setTopLevelNavigator(navigatorRef)
+        }}
+      />
+    </Provider>
+  )
 }
