cc26d30ba5fd8baff63253c78e2a1cef290f2acc
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2019-08-28 07:24:50 -0300

    Feed hooks e inline styles

--- src/screens/inicio/feed/index.js
+++ src/screens/inicio/feed/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import { View, FlatList, ActivityIndicator, InteractionManager } from 'react-native'
-import { connect } from 'react-redux'
+import React, { useEffect } from 'react'
+import { View, FlatList, ActivityIndicator, InteractionManager, StyleSheet } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
 
 import Prato from '~/src/components/Prato'
 import actions from '~/src/redux/actions/PratoActionCreator'
@@ -9,98 +9,93 @@ import { H5 } from '~/src/styled-components/Texto'
 import { Wrapper, WrapperCenter } from '~/src/styled-components/Wrapper'
 import AsyncStorage from '~/src/util/AsyncStorage'
 
-class Feed extends Component {
-  componentDidMount() {
+const Feed = props => {
+  const dispatch = useDispatch()
+  const { pratos, offset, limit, hasMore, erro, loading } = useSelector(state => state.PratoReducer)
+
+  useEffect(() => {
     InteractionManager.runAfterInteractions(async () => {
-      const token = await AsyncStorage.getItem('token')
-      if (!token) {
-        this.props.navigation.navigate('Home')
-      } else {
-        this.props.getPratos(this.props.offset, this.props.limit)
-      }
+      await getToken()
     })
+  }, [])
+
+  async function getToken() {
+    const token = await AsyncStorage.getItem('token')
+    if (!token) {
+      props.navigation.navigate('Home')
+    } else {
+      dispatch(actions.getPratos(offset, limit))
+    }
   }
 
-  renderFooter = () => (
-    <View style={{ alignItems: 'center' }}>
-      {this.props.hasMore ? (
-        <ActivityIndicator style={{ margin: 20 }} size="large" color="#1d3f72" />
-      ) : (
-        <H5 style={{ marginBottom: 80, color: '#CCC' }}>Não há mais pratos...</H5>
-      )}
-    </View>
-  )
-
-  renderItem = ({ item }) => (
-    <Prato
-      key={item.Id}
-      id={item.Id}
-      titulo={item.Nome}
-      descricao={item.Descricao}
-      imagem={item.Foto}
-      dificuldade={item.Dificuldade}
-      navegar={this.props.navigation.navigate}
-    />
-  )
-
-  render() {
+  function renderFooter() {
     return (
-      <Wrapper>
-        {this.props.loading ? (
-          <WrapperCenter>
-            <ActivityIndicator size="large" color="#1d3f72" />
-          </WrapperCenter>
+      <View style={styles.alignCenter}>
+        {hasMore ? (
+          <ActivityIndicator style={styles.margin20} size="large" color="#1d3f72" />
         ) : (
-          <View style={{ flex: 1 }}>
-            <View>
-              {this.props.erro !== '' && <H5 style={{ color: 'red' }}>{this.props.erro}</H5>}
-              <FlatList
-                data={this.props.pratos}
-                renderItem={this.renderItem}
-                keyExtractor={item => item.Id.toString()}
-                onEndReached={() => {
-                  this.props.hasMore
-                    ? this.props.getPratos(this.props.offset, this.props.limit)
-                    : ''
-                }}
-                onEndReachedThreshold={0.1}
-                ListFooterComponent={this.renderFooter}
-              />
-            </View>
-            <Botao
-              style={{
-                position: 'absolute',
-                bottom: 5,
-                right: 5,
-                width: 45,
-              }}
-              onPress={() => this.props.navigation.navigate('AddPrato')}
-            >
-              <BotaoTexto style={{ fontSize: 22 }}>+</BotaoTexto>
-            </Botao>
-          </View>
+          <H5 style={styles.noMoreMessage}>Não há mais pratos...</H5>
         )}
-      </Wrapper>
+      </View>
     )
   }
+
+  function renderItem({ item }) {
+    return (
+      <Prato
+        key={item.Id}
+        id={item.Id}
+        titulo={item.Nome}
+        descricao={item.Descricao}
+        imagem={item.Foto}
+        dificuldade={item.Dificuldade}
+        navegar={props.navigation.navigate}
+      />
+    )
+  }
+  return (
+    <Wrapper>
+      {loading ? (
+        <WrapperCenter>
+          <ActivityIndicator size="large" color="#1d3f72" />
+        </WrapperCenter>
+      ) : (
+        <View style={styles.container}>
+          <View>
+            {erro !== '' && <H5 style={styles.erro}>{erro}</H5>}
+            <FlatList
+              data={pratos}
+              renderItem={renderItem}
+              keyExtractor={item => item.Id.toString()}
+              onEndReached={() => {
+                if (hasMore) dispatch(actions.getPratos(offset, limit))
+              }}
+              onEndReachedThreshold={0.1}
+              ListFooterComponent={renderFooter}
+            />
+          </View>
+          <Botao style={styles.btnAddPrato} onPress={() => props.navigation.navigate('AddPrato')}>
+            <BotaoTexto style={styles.btnAddPratoText}>+</BotaoTexto>
+          </Botao>
+        </View>
+      )}
+    </Wrapper>
+  )
 }
 
-const mapStateToProps = state => ({
-  pratos: state.PratoReducer.pratos,
-  offset: state.PratoReducer.offset,
-  limit: state.PratoReducer.limit,
-  hasMore: state.PratoReducer.hasMore,
-  erro: state.PratoReducer.erro,
-  loading: state.PratoReducer.loading,
-})
-
-const mapDispatchToProps = dispatch => ({
-  getPratos(offset, limit) {
-    dispatch(actions.getPratos(offset, limit))
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  alignCenter: { alignItems: 'center' },
+  margin20: { margin: 20 },
+  noMoreMessage: { marginBottom: 80, color: '#CCC' },
+  erro: { color: 'red' },
+  btnAddPrato: {
+    position: 'absolute',
+    bottom: 5,
+    right: 5,
+    width: 45,
   },
+  btnAddPratoText: { fontSize: 22 },
 })
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Feed)
+export default Feed
